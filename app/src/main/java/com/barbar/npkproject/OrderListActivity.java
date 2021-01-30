a401ea1756d8446ef2c0720043bbb8a3edb8a19b
Eugene <evgenedar@mail.ru>
2021-01-30 11:39:08 +0300

    ListView added

--- app/src/main/java/com/barbar/npkproject/OrderListActivity.java
+++ app/src/main/java/com/barbar/npkproject/OrderListActivity.java
@@ -6,8 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -18,6 +23,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class OrderListActivity extends AppCompatActivity {
@@ -25,14 +32,19 @@ public class OrderListActivity extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("orders_list");
 
-    TextView textView;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_list);
 
-        textView = findViewById(R.id.temp_text);
+        listView = findViewById(R.id.list_view);
+        List<String> list_of_orders = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list_of_orders);
+
+        listView.setAdapter(adapter);
 
         myRef.addChildEventListener(new ChildEventListener() {
             @SuppressLint("SetTextI18n")
@@ -41,18 +53,17 @@ public class OrderListActivity extends AppCompatActivity {
                 try {
                     JSONObject object = new JSONObject(Objects.requireNonNull(snapshot.getValue(String.class)));
                     if (object.get("comments").toString().length() > 5) {
-                        textView.setText(textView.getText() + "\n\n" +
-                                object.get("items").toString() + "\nАдрес: " +
+                        list_of_orders.add(object.get("items").toString() + "\nАдрес: " +
                                 object.get("address").toString() + "\nКомментарий: " +
                                 object.get("comments").toString());
                     } else {
-                        textView.setText(textView.getText() + "\n\n" +
-                                object.get("items").toString() + "\nАдрес: " +
+                        list_of_orders.add(object.get("items").toString() + "\nАдрес: " +
                                 object.get("address").toString());
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
+                adapter.notifyDataSetChanged();
             }
             @Override
             public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) { }
@@ -62,6 +73,12 @@ public class OrderListActivity extends AppCompatActivity {
             public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) { }
             @Override
             public void onCancelled(@NonNull DatabaseError error) { }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), view.toString(), Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
