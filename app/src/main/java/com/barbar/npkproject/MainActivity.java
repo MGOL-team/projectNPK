cbcc7dba383061d4dbe4fa9962d52ccc04b970eb
Vseslav Demidovich <vseslav2244@gmail.com>
2021-01-30 23:01:02 +0300

    Marks history added

--- app/src/main/java/com/barbar/npkproject/MainActivity.java
+++ app/src/main/java/com/barbar/npkproject/MainActivity.java
@@ -1,5 +1,7 @@
 package com.barbar.npkproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -7,19 +9,27 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
     Button sendButton;
     TextView textLoginName;
     TextView textViewUser;
+
+    ListView listView;
 
     ImageButton toDoOrderButton;
     ImageButton toCheckOrdersButton;
@@ -51,7 +63,36 @@ public class MainActivity extends AppCompatActivity {
         textViewUser = findViewById(R.id.text_edit_user);
         toDoOrderButton = findViewById(R.id.to_do_order_button);
         toCheckOrdersButton = findViewById(R.id.to_check_orders_button);
+        listView = findViewById(R.id.list_view);
         goToSettingsButton = findViewById(R.id.settings_button);
+
+        List<String> list_of_ratings = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list_of_ratings);
+
+        listView.setAdapter(adapter);
+
+        myRef.child(getLogin()).child("rating").addChildEventListener(new ChildEventListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                try {
+                    JSONObject object = new JSONObject(Objects.requireNonNull(snapshot.getValue(String.class)));
+                    list_of_ratings.add(object.get("value") + " — " + object.get("login"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                adapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) { }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) { }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) { }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) { }
+        });
 
         toDoOrderButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         goToSettingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
